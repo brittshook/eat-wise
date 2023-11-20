26ee19614c49e4b5193dd133b8d21766dcca7a25
Britt Shook <brittanysshook@gmail.com>
2023-11-20 14:56:58 -0500

    add script for displaying relevant fields if user is existing (mock version: existing@gmail.com)

---
+++ scripts/login.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const buttonWithEmail = document.querySelector('#with-email');
+
+    buttonWithEmail.addEventListener("click", ()  => {
+        const emailInput =  document.querySelector('#email');
+        let value = emailInput.value;
+        value = value.trim();
+
+        if (value === 'existing@gmail.com') {
+            const logInSignUp = document.querySelector('#login-or-signup');
+            const welcomeBack = document.querySelector('#welcome-back');
+            const passwordField = document.querySelector('label[for="password"]');
+            const buttonLogIn = document.querySelector('#login');
+            
+            buttonWithEmail.style.display = 'none';
+            logInSignUp.style.display = 'none';
+            passwordField.style.display = 'flex';
+            welcomeBack.style.display = 'block';
+            buttonLogIn.style.display = 'block';
+        } else {
+
+        }
+
+    })
+})
